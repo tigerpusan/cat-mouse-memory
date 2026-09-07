d97975e1ff671eb2afaba9bfa19df197b34e1b74
tigerpusan <tigerpusan74@gmail.com>
2026-09-07 11:41:47 +0900

    Add offline cache

---
+++ www/service-worker.js
@@ -0,0 +1,1 @@
+const CACHE='cat-puzzle-v1';const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
